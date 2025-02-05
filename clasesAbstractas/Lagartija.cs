59b213fdea7d8d35cec251e1fb1f40897f389a6f
Ana Galvez <anamariagalvez01@gmail.com>
2025-02-05 03:24:50 -0300

    feat: lagartija lista

--- clasesAbstractas/Lagartija.cs
+++ clasesAbstractas/Lagartija.cs
@@ -3,8 +3,18 @@
 
 namespace clasesAbstractas
 {
-    class Lagartija
+    class Lagartija : Animales
     {
+        private String nombre;
 
+        public Lagartija(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public override void GetNombre()
+        {
+            Console.WriteLine("El nombre de la lagartija es: " + nombre);
+        }
     }
 }
