fac47ec1c91d5d88b2575aab1d71c35995962fca
Ana Galvez <anamariagalvez01@gmail.com>
2025-02-05 02:53:51 -0300

    feat: clase animales

---
+++ clasesAbstractas/Animales.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace clasesAbstractas
+{
+    class Animales
+    {
+        String nombre;
+
+        public Animales(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public void Respirar()
+        {
+            Console.WriteLine("Soy capaz de respirar");
+        }
+        public void getNombre()
+        {
+            Console.WriteLine("El nombre del animal es: "+nombre);
+        }
+
+    }
+}
